6d2f79632ac915a00e5fc93e917f348f3528918b
Mensah Shadrach <Redhand@mensahs-mbp.mshome.net>
2016-10-31 14:27:45 +0000

    Initial Commit

---
+++ CWStreaks/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CWStreaks
+//
+//  Created by Mensah Shadrach on 10/31/16.
+//  Copyright © 2016 Mensah Shadrach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CWStreaks/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CWStreaks
+//
+//  Created by Mensah Shadrach on 10/31/16.
+//  Copyright © 2016 Mensah Shadrach. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
